4ff4f8eaf9ce1202f56eb302e878a8215e3ab14f
Ondra Hlaváč <ondra@hlavac.cz>
2025-07-09 12:12:39 +0200

    feat: prepare the repo for Netlify deploy

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,16 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: [vitePreprocess(), mdsvex()],
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+	}) },
 	extensions: ['.svelte', '.svx']
 };
 
