babae572d157424346ad71b83819429f8ffe601b
Ondra Hlaváč <ondra@hlavac.cz>
2025-07-22 12:45:56 +0200

    feat: add a race logo in the header

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-
 </script>
 
 
@@ -8,7 +7,7 @@
     <div class="flex lg:flex-1">
       <a href="/" class="-m-1.5 p-1.5">
         <span class="sr-only">Slapy Trail Maniacs</span>
-        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" class="h-8 w-auto" />
+        <img src="/slapy-trail-maniacs-2025-logo.svg" alt="" class="h-20 w-auto" />
       </a>
     </div>
     <div class="flex lg:hidden">
@@ -36,9 +35,9 @@
     <div class="fixed inset-0 z-50"></div>
     <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
       <div class="flex items-center justify-between">
-        <a href="#" class="-m-1.5 p-1.5">
+        <a href="/" class="-m-1.5 p-1.5">
           <span class="sr-only">Slapy Trail Maniacs</span>
-          <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="" class="h-8 w-auto" />
+          <img src="/slapy-trail-maniacs-2025-logo.svg" alt="" class="h-20 w-auto" />
         </a>
         <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
           <span class="sr-only">Zavřít menu</span>
