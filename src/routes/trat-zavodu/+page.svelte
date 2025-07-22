c689d27c6c14c12ed37c587a937cea658adaa040
Ondra Hlaváč <ondra@hlavac.cz>
2025-07-22 16:26:47 +0200

    feat: add link to course maps at mapy.cz

--- src/routes/trat-zavodu/+page.svelte
+++ src/routes/trat-zavodu/+page.svelte
@@ -1,6 +1,3 @@
 <h1>Trať závodu</h1>
-<p>chceme málo asfaltu</p>
-<h2>Mapa Slapy Trail Maniacs 2025</h2>
-<p>Mapa závodu bude zveřejněna v den závodu.</p>
-<h2>GPX ke stažení</h2>
-<span class="text-sm">Partneři</span>
+
+<a class="text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline-indigo-600 inline-block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 w-15" href="https://mapy.com/s/pagajeceka">Mapy jednotlivých tratí Slapy Trail Maniacs 2025</a>
